e2515d6559767be6c6aa44b8d32ee92491022e61
Marc Robin Gruener <gmarcrobin@outlook.de>
2022-02-06 17:45:35 +0100

    Making it TS

--- nextjs-blog/pages/sitemap.xml.tsx
+++ nextjs-blog/pages/sitemap.xml.tsx
@@ -1,3 +1,5 @@
+import { GetServerSideProps } from 'next';
+
 const EXTERNAL_DATA_URL = 'https://testurl.com/'
 
 function generateSiteMap(posts) {
@@ -27,16 +29,31 @@ function SiteMap() {
   // getServerSideProps will do the heavy lifting
 }
 
-export async function getServerSideProps({ res }) {
-  // We make an API call to gather the URLs for our site
+// export async function getServerSideProps({ res }) {
+//   // We make an API call to gather the URLs for our site
+//   const request = await fetch(EXTERNAL_DATA_URL)
+//   const posts = await request.json()
+
+//   // We generate the XML sitemap with the posts data
+//   const sitemap = generateSiteMap(posts)
+
+//   res.setHeader('Content-Type', 'text/xml')
+//   // we send the XML to the browser
+//   res.write(sitemap)
+//   res.end()
+
+//   return {
+//     props: {}
+//   }
+// }
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const request = await fetch(EXTERNAL_DATA_URL)
   const posts = await request.json()
 
-  // We generate the XML sitemap with the posts data
   const sitemap = generateSiteMap(posts)
 
   res.setHeader('Content-Type', 'text/xml')
-  // we send the XML to the browser
   res.write(sitemap)
   res.end()
 
